e54afb8043019455fbff413d13b06998358ca21d
Veasna <veasnahoorne@hotmail.com>
2023-12-04 18:07:03 +0100

    ✨ F01: LOGIN > Create LoginPage

--- src/App.jsx
+++ src/App.jsx
@@ -1,79 +1,10 @@
 import { useState } from "react";
-
-// function App() {
-//   //state (état,données)
-//   const [firstName, setFirstName] = useState("");
-
-//   //comportement
-//   const handleChange = (event) => {
-//     setFirstName(event.target.value);
-//     console.log(firstName);
-//   };
-
-//   const handleSubmit = (event) => {
-//     event?.preventDefault();
-
-//     if (!firstName) {
-//       alert("Veuillez entrer un prénom");
-//     } else {
-//       alert(`Bonjour ${firstName}`);
-//     }
-//     setFirstName("");
-//   };
-
-//   //affichage (render)
-//   return (
-//     <div>
-//       <h1>Bienvenue chez nous !</h1>
-//       <h2>Connectez-vous</h2>
-
-//       <form onSubmit={handleSubmit}>
-//         <input
-//           value={firstName}
-//           onChange={handleChange}
-//           placeholder="Entrez votre prénom..."
-//           type="text"
-//         />
-//         <button type="submit">Accédez à votre espace</button>
-//       </form>
-//     </div>
-//   );
-// }
+import LoginPage from "./components/pages/LoginPage";
 
 function App() {
-  const [firstName, setFirstName] = useState("");
-  //données/déclaration
 
-  const handleChange = (event) => {
-    setFirstName(event.target.value);
-    console.log(firstName);
-  };
+  return <LoginPage/>
 
-  const handleSubmit = (event) => {
-    if (firstName) {
-      alert(`Bonjour ${firstName}`);
-    } else {
-      alert("Veuillez entrer un prénom");
-    }
-    setFirstName("");
-  };
-
-  //affichage
-  return (
-    <div>
-      <h1>Bienvenue chez nous !</h1>
-      <h2>Connectez-vous</h2>
-      <form onSubmit={handleSubmit}>
-        <input
-          value={firstName}
-          onChange={handleChange}
-          type="text"
-          placeholder="Entrez votre prénom..."
-        />
-        <button>Accédez à votre espace</button>
-      </form>
-    </div>
-  );
-}
+  }
 
 export default App;
