7fba0f1ec8027713071f743bb289802182a1a7a2
Veasna <veasnahoorne@hotmail.com>
2023-12-12 16:19:25 +0100

    ✨ F02: App >Install routing system

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
 import { useState } from "react";
 import LoginPage from "./components/pages/login/LoginPage";
+import { Route, Routes } from "react-router-dom";
+import OrderPage from "./components/pages/order/OrderPage";
+import ErrorPage from "./components/pages/error/ErrorPage";
 
 
 function App() {
 
-  return <LoginPage />
+  return(
+  <Routes>
+    <Route path="/" element={<LoginPage />} />
+    <Route path="/order" element={<OrderPage />} />
+    <Route path="*" element={<ErrorPage />} />
+  </Routes>
+  ) 
+  
 
   }
 
---
+++ src/components/pages/order/OrderPage.jsx
@@ -0,0 +1,12 @@
+
+
+export default function OrderPage() 
+{
+    return(
+    <div>
+<h1>Hellow World !</h1>
+    </div>
+
+    )
+
+}
