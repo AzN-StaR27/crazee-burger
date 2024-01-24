10275a434b88667a33d2297e13b6e65dcc2f35d8
Veasna <114160097+AzN-StaR27@users.noreply.github.com>
2024-01-24 12:09:54 +0100

    F03: LOGIN > Add styling and background

--- src/components/pages/login/LoginPage.jsx
+++ src/components/pages/login/LoginPage.jsx
@@ -16,11 +16,26 @@ export default function LoginPage() {
 }
 
 const LoginPageStyled = styled.div`
-  background: url(F03/F03_burger-background.jpg) no-repeat center/cover;
   height: 100vh;
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
   gap: 50px;
+
+  ::before {
+    content: "";
+    background: url("/images/burger-and-fries-background.jpg")
+      rgba(0, 0, 0, 0.7);
+    background-size: cover;
+    background-position: center;
+    background-blend-mode: darken;
+
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    z-index: -1;
+  }
 `;
