35e5def39e22cee30ec08803094c905e224b1497
Veasna <114160097+AzN-StaR27@users.noreply.github.com>
2024-02-22 18:15:52 +0100

    ✨ F04: ORDER > Navbar > trigger reload on click

--- src/components/reusable-ui/Logo.jsx
+++ src/components/reusable-ui/Logo.jsx
@@ -1,9 +1,9 @@
 import styled from "styled-components";
 import { theme } from "../../theme";
 
-export default function Logo({ className }) {
+export default function Logo({ className, onClick }) {
   return (
-    <LogoStyled className={className}>
+    <LogoStyled className={className} onClick={onClick}>
       <h1 href="#">crazee</h1>
       <img src="\images\logo-orange.png" alt="logo crazee-burger" />
       <h1 href="#">burger</h1>
