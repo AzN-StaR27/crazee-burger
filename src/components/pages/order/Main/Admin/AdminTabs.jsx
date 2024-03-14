b2e90e632490143a03516ed8afc00fb2a0f6b98f
Veasna <114160097+AzN-StaR27@users.noreply.github.com>
2024-03-14 15:17:11 +0100

    ✨ F07 : ADMIN > Tab > Add label

--- src/components/pages/order/Main/Admin/AdminTabs.jsx
+++ src/components/pages/order/Main/Admin/AdminTabs.jsx
@@ -2,16 +2,24 @@ import React from "react";
 import styled from "styled-components";
 import Tab from "../../../../reusable-ui/Tab";
 import { FiChevronDown, FiChevronUp } from "react-icons/fi";
+import { AiOutlinePlus } from "react-icons/ai";
 import { useState } from "react";
 import { theme } from "../../../../../theme";
 export default function AdminTabs({ isCollapsed, setIsCollapsed }) {
   return (
     <AdminTabsStyled>
       <Tab
+        label=""
         Icon={isCollapsed ? <FiChevronUp /> : <FiChevronDown />}
         onClick={() => {
           setIsCollapsed(!isCollapsed);
         }}
+        className={isCollapsed ? "is-active" : ""}
+      />
+      <Tab
+        Icon={<AiOutlinePlus />}
+        label={"Ajouter un produit"}
+        onClick={() => setIsCollapsed(!isCollapsed)}
         className={isCollapsed ? "is-active" : ""}
       />
     </AdminTabsStyled>
@@ -26,5 +34,9 @@ const AdminTabsStyled = styled.div`
     background-color: ${theme.colors.background_dark};
     color: ${theme.colors.white};
     border-color: ${theme.colors.background_dark};
+  }
+
+  button {
+    margin-left: 1px;
   }
 `;
--- src/components/pages/order/OrderPage.jsx
+++ src/components/pages/order/OrderPage.jsx
@@ -9,7 +9,7 @@ import { useState } from "react";
 export default function OrderPage() {
   //state
 
-  const [isModeAdmin, setIsModeAdmin] = useState(true);
+  const [isModeAdmin, setIsModeAdmin] = useState(false);
 
   //comportement
 
