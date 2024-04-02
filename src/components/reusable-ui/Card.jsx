dba4c3fb539e0f2c5ea5d5083563c05db43a674e
Veasna <114160097+AzN-StaR27@users.noreply.github.com>
2024-04-02 11:50:24 +0200

    ✨ F08 : CARD > Add deleteButton (style only)

--- src/components/reusable-ui/Card.jsx
+++ src/components/reusable-ui/Card.jsx
@@ -2,10 +2,14 @@ import styled from "styled-components";
 import PrimaryButton from "./PrimaryButton";
 import { formatPrice } from "../../utils/maths";
 import { theme } from "../../theme";
+import { TiDelete } from "react-icons/ti";
 
 export default function Card({ imageSource, title, leftDescription }) {
   return (
     <CardStyled className="produit">
+      <button className="delete-button" aria-label="delete-button">
+        <TiDelete className="icon" />
+      </button>
       <div className="image">
         <img src={imageSource} alt={title} />
       </div>
@@ -33,6 +37,33 @@ const CardStyled = styled.div`
   padding-bottom: 10px;
   box-shadow: ${theme.shadows.medium};
   border-radius: ${theme.borderRadius.extraRound};
+  position: relative;
+
+  .delete-button {
+    border: 1px solid red;
+    position: absolute;
+    top: 15px;
+    right: 15px;
+    cursor: pointer;
+    width: 30px;
+    height: 30px;
+    color: ${theme.colors.primary};
+    z-index: 2;
+    padding: 0;
+    border: none;
+    background: none;
+
+    .icon {
+      height: 100%;
+      width: 100%;
+    }
+    :hover {
+      color: ${theme.colors.red};
+    }
+    :active {
+      color: ${theme.colors.primary};
+    }
+  }
 
   .image {
     width: 100%;
