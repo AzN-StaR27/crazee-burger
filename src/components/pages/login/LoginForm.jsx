9b6ecee76ed98d3f7e4526e63a6002e9768353f9
Veasna <114160097+AzN-StaR27@users.noreply.github.com>
2024-04-02 13:10:54 +0200

    ✨ F08 : TextInput > Create common base to both version

--- src/components/pages/login/LoginForm.jsx
+++ src/components/pages/login/LoginForm.jsx
@@ -41,6 +41,7 @@ export default function LoginForm() {
           required
           Icon={<BsPersonCircle />}
           className="input-login"
+          version="normal"
         />
 
         <PrimaryButton
--- src/components/pages/order/Main/Admin/AdminPanel/AddForm.jsx
+++ src/components/pages/order/Main/Admin/AdminPanel/AddForm.jsx
@@ -2,7 +2,11 @@ import React, { useContext, useState } from "react";
 import styled from "styled-components";
 import OrderContext from "../../../../../../context/OrderContext";
 import { FiCheck } from "react-icons/fi";
+import { FaHamburger } from "react-icons/fa";
+import { BsFillCameraFill } from "react-icons/bs";
+import { MdOutlineEuro } from "react-icons/md";
 import { theme } from "../../../../../../theme";
+import TextInput from "../../../../../reusable-ui/TextInput.jsx";
 
 const EMPTY_PRODUCT = {
   id: "",
@@ -51,26 +55,32 @@ export default function AddForm() {
         )}
       </div>
       <div className="input-fields">
-        <input
+        <TextInput
           name="title"
           value={newProduct.title}
-          onChange={handleChange}
           type="text"
           placeholder="Nom du produit (ex: Super Burger)"
+          onChange={handleChange}
+          Icon={<FaHamburger />}
+          version="minimalist"
         />
-        <input
+        <TextInput
           name="imageSource"
           value={newProduct.imageSource}
-          onChange={handleChange}
           type="text"
           placeholder="Lien URL d'une image (ex: https://la-photo-de-mon-produit.png) "
+          onChange={handleChange}
+          Icon={<BsFillCameraFill />}
+          version="minimalist"
         />
-        <input
+        <TextInput
           name="price"
           value={newProduct.price ? newProduct.price : ""}
-          onChange={handleChange}
           type="text"
           placeholder="Prix"
+          onChange={handleChange}
+          Icon={<MdOutlineEuro />}
+          version="minimalist"
         />
       </div>
       <div className="submit">
@@ -121,9 +131,11 @@ const AddFormStyled = styled.form`
     }
   }
   .input-fields {
-    background: blue;
+    /* background: blue; */
     grid-area: 1 / 2 / 4 / 3;
+
     display: grid;
+    grid-row-gap: 8px;
   }
   .submit {
     background-color: green;
