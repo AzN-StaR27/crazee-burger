03e083ad436d8a27a39c8224d4ac876d4d4bc5a1
Veasna <114160097+AzN-StaR27@users.noreply.github.com>
2024-04-02 19:31:24 +0200

    ⚗️ F08 : Button experiments (with design system)

--- src/components/reusable-ui/Button.jsx
+++ src/components/reusable-ui/Button.jsx
@@ -1,12 +1,17 @@
 import styled, { css } from "styled-components";
 import { theme } from "../../theme";
 
+type size = "small" | "regular" | "large";
+type state = "clicked" | "focused" | "hovered" | "disabled";
+
 export default function Button({
   className,
   label,
   Icon,
   version = "normal",
   onClick,
+  size,
+  state,
 }) {
   return (
     <ButtonStyled className={className} version={version} onClick={onClick}>
@@ -20,69 +25,76 @@ const ButtonStyled = styled.button`
   /* ${(props) => props.version === "normal" && extraStylePrimary};
   ${(props) => props.version === "success" && extraStyleSuccess}; */
 
-  ${({ version }) => extraStyle[version]};
+  ${({ version, size }) => getButtonStyle(version, size)};
 `;
 
-const extraStylePrimary = css`
-  width: 100%;
-  /* border: 1px solid red; */
-  display: inline-flex;
-  justify-content: center;
-  align-items: center;
-  position: relative;
-  white-space: nowrap;
-  text-decoration: none;
-  line-height: 1;
+const getButtonStyle = (version, size) => {
+  if (version === "primary") return getExtraStylePrimary(size, state);
+  if (version === "success") return getExtraStyleSuccess(size, state);
+};
 
-  padding: 18px 24px;
-  border-radius: 5px;
-  font-size: 15px;
-  font-weight: 800;
-  color: white;
-  background-color: #ff9f1b;
-  border: 1px solid #ff9f1b;
-
-  &:hover:not(:disabled) {
-    background-color: ${theme.colors.white};
-    color: ${theme.colors.primary};
-    border: 1px solid ${theme.colors.primary};
-    transition: all 200ms ease-out;
-  }
-
-  &:active {
-    color: ${theme.colors.white};
-    background-color: #ff9f1b;
-  }
-
-  &.is-disabled {
-    opacity: 50%;
-    cursor: not-allowed;
-    z-index: 2;
-  }
-
-  &.with-focus {
-    border: 1px solid white;
-    background-color: ${theme.colors.white};
-    color: ${theme.colors.primary};
-    :hover {
-      color: ${theme.colors.white};
-      background-color: ${theme.colors.primary};
-      border: 1px solid ${theme.colors.white};
-    }
-    :active {
-      background-color: ${theme.colors.white};
-      color: ${theme.colors.primary};
-    }
-  }
-
-  .icon {
-    display: flex;
+const getExtraStylePrimary = (size, state) => {
+  return css`
+    width: 100%;
+    /* border: 1px solid red; */
+    display: inline-flex;
     justify-content: center;
     align-items: center;
-  }
-`;
+    position: relative;
+    white-space: nowrap;
+    text-decoration: none;
+    line-height: 1;
 
-const extraStyleSuccess = css`
+    padding: 18px 24px;
+    border-radius: 5px;
+    font-size: ${sizesOptions[size]};
+    font-weight: 800;
+    color: white;
+    background-color: #ff9f1b;
+    border: ${BORDER_COLORS[state]};
+
+    &:hover:not(:disabled) {
+      background-color: ${theme.colors.white};
+      color: ${theme.colors.primary};
+      border: 1px solid ${theme.colors.primary};
+      transition: all 200ms ease-out;
+    }
+
+    &:active {
+      color: ${theme.colors.white};
+      background-color: #ff9f1b;
+    }
+
+    &.is-disabled {
+      opacity: 50%;
+      cursor: not-allowed;
+      z-index: 2;
+    }
+
+    &.with-focus {
+      border: 1px solid white;
+      background-color: ${theme.colors.white};
+      color: ${theme.colors.primary};
+      :hover {
+        color: ${theme.colors.white};
+        background-color: ${theme.colors.primary};
+        border: 1px solid ${theme.colors.white};
+      }
+      :active {
+        background-color: ${theme.colors.white};
+        color: ${theme.colors.primary};
+      }
+    }
+
+    .icon {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+    }
+  `;
+};
+
+const getExtraStyleSuccess = css`
   cursor: pointer;
   color: ${theme.colors.white};
   background: ${theme.colors.success};
@@ -102,7 +114,21 @@ const extraStyleSuccess = css`
     border: 1px solid ${theme.colors.success};
   }
 `;
+
 const extraStyle = {
   primary: extraStylePrimary,
   success: extraStyleSuccess,
+};
+
+const sizesOptions = {
+  small: extraStyleSmall,
+  normal: extraStyleNormal,
+  large: extraStyleLarge,
+};
+
+const BORDER_COLORS = {
+  clicked: "grey",
+  enabled: "blue",
+  focused: "white",
+  disabled: "black",
 };
