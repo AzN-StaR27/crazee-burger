f98a5c9255ecb8bd8fe1f96057153916e1a6f48a
Veasna <114160097+AzN-StaR27@users.noreply.github.com>
2024-04-18 19:55:29 +0200

    🚧 F14 : BASKET > add animation on products added or deleted

--- src/components/pages/order/Main/Basket/BasketProducts.jsx
+++ src/components/pages/order/Main/Basket/BasketProducts.jsx
@@ -5,7 +5,7 @@ import { IMAGE_COMING_SOON } from "../../../../../enums/product";
 import { findObjectById } from "../../../../../utils/array";
 import OrderContext from "../../../../../context/OrderContext.jsx";
 import { checkIfProductIsClicked } from "../MainRightSide/Menu/helper.jsx";
-
+import { TransitionGroup, CSSTransition } from "react-transition-group";
 export default function BasketProducts() {
   const {
     username,
@@ -24,33 +24,43 @@ export default function BasketProducts() {
 
   return (
     <BasketProductsStyled>
-      {basket.map((basketProduct) => {
-        const menuProduct = findObjectById(basketProduct.id, menu);
-        return (
-          <div className="basket-card" key={basketProduct.id}>
-            <BasketCard
-              {...menuProduct}
-              imageSource={
-                menuProduct.imageSource
-                  ? menuProduct.imageSource
-                  : IMAGE_COMING_SOON
-              }
-              quantity={basketProduct.quantity}
-              onDelete={(event) => handleOnDelete(event, basketProduct.id)}
-              isClickable={isModeAdmin}
-              onClick={
-                isModeAdmin
-                  ? () => handleProductSelected(basketProduct.id)
-                  : null
-              }
-              isSelected={checkIfProductIsClicked(
-                basketProduct.id,
-                productSelected.id
-              )}
-            />
-          </div>
-        );
-      })}
+      <TransitionGroup>
+        {basket.map((basketProduct) => {
+          const menuProduct = findObjectById(basketProduct.id, menu);
+          return (
+            <CSSTransition
+              appear={true}
+              classNames={"abricot"}
+              key={basketProduct.id}
+              timeout={500}
+            >
+              <div className="basket-card">
+                <BasketCard
+                  {...menuProduct}
+                  imageSource={
+                    menuProduct.imageSource
+                      ? menuProduct.imageSource
+                      : IMAGE_COMING_SOON
+                  }
+                  quantity={basketProduct.quantity}
+                  onDelete={(event) => handleOnDelete(event, basketProduct.id)}
+                  isClickable={isModeAdmin}
+                  onClick={
+                    isModeAdmin
+                      ? () => handleProductSelected(basketProduct.id)
+                      : null
+                  }
+                  isSelected={checkIfProductIsClicked(
+                    basketProduct.id,
+                    productSelected.id
+                  )}
+                  className="pomme"
+                />
+              </div>
+            </CSSTransition>
+          );
+        })}
+      </TransitionGroup>
     </BasketProductsStyled>
   );
 }
@@ -61,7 +71,50 @@ const BasketProductsStyled = styled.div`
   display: flex;
   flex-direction: column;
   overflow-y: auto;
+  .abricot-appear {
+    .pomme {
+      transform: translateX(100px);
+      opacity: 0%;
+    }
+  }
 
+  .abricot-appear-active {
+    .pomme {
+      transition: 0.5s;
+      transform: translateX(0);
+      opacity: 100%;
+    }
+  }
+
+  .abricot-enter {
+    .pomme {
+      transform: translateX(100px);
+      opacity: 0%;
+    }
+  }
+
+  .abricot-enter-active {
+    .pomme {
+      transition: 0.5s;
+      transform: translateX(0);
+      opacity: 100%;
+    }
+  }
+
+  .abricot-exit {
+    .pomme {
+      transform: translateX(0);
+      opacity: 100%;
+    }
+  }
+
+  .abricot-exit-active {
+    .pomme {
+      transform: translateX(-100px);
+      opacity: 0%;
+      transition: 0.5s;
+    }
+  }
   .basket-card {
     margin: 10px 16px;
   }
