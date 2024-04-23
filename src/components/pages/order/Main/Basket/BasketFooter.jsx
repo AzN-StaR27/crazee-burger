c8c26307114094d83d7ea518af81a84e462dd20a
Veasna <114160097+AzN-StaR27@users.noreply.github.com>
2024-04-23 16:06:24 +0200

    ♻️ F14 : BASKET > Create BasketFooter

---
+++ src/components/pages/order/Main/Basket/BasketFooter.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import styled from "styled-components";
+import Header from "./Header";
+import { theme } from "../../../../../theme";
+
+export default function BasketFooter() {
+  return (
+    <Header>
+      <BasketFooterStyled>
+        <span>Codé avec ❤️ et React.JS</span>
+      </BasketFooterStyled>
+    </Header>
+  );
+}
+
+const BasketFooterStyled = styled.div`
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  span {
+    font-size: ${theme.fonts.size.P2};
+    font-family: ${theme.fonts.family.stylish};
+    font-weight: ${theme.fonts.weights.bold};
+    color: ${theme.colors.white};
+  }
+`;
