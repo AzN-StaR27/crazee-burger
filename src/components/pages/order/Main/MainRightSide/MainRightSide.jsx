200274074ade2d2534ba96059e542af8094c0266
Veasna <114160097+AzN-StaR27@users.noreply.github.com>
2024-04-23 14:09:03 +0200

    💄 F14 : ADMIN > Panel > add animation onAppear

--- src/components/pages/order/Main/MainRightSide/MainRightSide.jsx
+++ src/components/pages/order/Main/MainRightSide/MainRightSide.jsx
@@ -4,6 +4,8 @@ import OrderContext from "../../../../../context/OrderContext.jsx";
 import { theme } from "../../../../../theme/index.js";
 import Admin from "./Admin/Admin.jsx";
 import Menu from "./Menu/Menu.jsx";
+import { CSSTransition, TransitionGroup } from "react-transition-group";
+import { adminAnimation } from "../../../../../theme/animations.jsx";
 
 export default function MainRightSide() {
   const { isModeAdmin } = useContext(OrderContext);
@@ -11,7 +13,13 @@ export default function MainRightSide() {
   return (
     <MainRightSideStyled>
       <Menu />
-      {isModeAdmin && <Admin />}
+      {isModeAdmin && (
+        <TransitionGroup className="transition-group">
+          <CSSTransition appear={true} classNames="admin" timeout={500}>
+            <Admin />
+          </CSSTransition>
+        </TransitionGroup>
+      )}
     </MainRightSideStyled>
   );
 }
@@ -22,4 +30,6 @@ const MainRightSideStyled = styled.div`
   display: grid;
   /* border-bottom-left-radius: ${theme.borderRadius.extraRound}; */
   border-bottom-right-radius: ${theme.borderRadius.extraRound};
+
+  ${adminAnimation}
 `;
